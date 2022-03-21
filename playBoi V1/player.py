96abf1db5a77c4937aad1a2bd87ea1953640d353
Debyte <74857321+DebyO2@users.noreply.github.com>
2022-03-21 23:30:09 +0530

    Update player.py

--- playBoi V1/player.py
+++ playBoi V1/player.py
@@ -4,8 +4,27 @@ from win32gui import GetWindowText, GetForegroundWindow
 pygame.init()
 running = True
 paused = False
+name = GetWindowText(GetForegroundWindow())
 
 def toggled():
+    global paused
+    global name
+    if(GetWindowText(GetForegroundWindow()) == name):
+        if pygame.mixer.music.get_busy():
+            pygame.mixer.music.pause()
+            paused = True
+        else:
+            pygame.mixer.music.unpause()
+            paused = False        
+    
+def leave():
+    global running
+    global name
+    if(GetWindowText(GetForegroundWindow()) == name):
+        running = False
+        pygame.mixer.music.stop()
+
+def toggledchk():
     global paused
     if pygame.mixer.music.get_busy():
         pygame.mixer.music.pause()
@@ -14,29 +33,31 @@ def toggled():
         pygame.mixer.music.unpause()
         paused = False        
     
-    
+def leavechk():
+    global running
+    running = False
+    pygame.mixer.music.stop()
 
 def playSong(path : str,volume : int,Toloop : int):
-    print("spacebar : pause/unpause")
-    print("esc : stop")
-    name = GetWindowText(GetForegroundWindow())
+    print("spacebar         : pause/unpause (within the window)")
+    print("shift + spacebar : pause/unpause (global shortcut)")
+    print("esc              : stop          (within the window)")
+    print("shift + esc      : stop          (global shortcut)\n")
+    pygame.mixer.music.load(path)
     pygame.mixer.music.load(path)
     pygame.mixer.music.set_volume(volume)
     pygame.mixer.music.play(Toloop)
+    keyboard.add_hotkey('space', toggled)
+    keyboard.add_hotkey('escape', leave)
+    keyboard.add_hotkey('shift+space', toggledchk)
+    keyboard.add_hotkey('shift+escape', leavechk)
     while running:
         if paused or pygame.mixer.music.get_busy():
             pass
         else:
-            print("finished playing")
             break
-
-        event = keyboard.read_event()
-        if(GetWindowText(GetForegroundWindow()) == name):
-            if event.event_type == keyboard.KEY_DOWN and event.name == 'space':
-                toggled()
-            if event.event_type == keyboard.KEY_DOWN and event.name == 'esc':
-                pygame.mixer.music.stop()
-                break
+    print("finished playing")
+        
     
 if __name__ == '__main__':
     porth = input("Enter the path of the song: ")

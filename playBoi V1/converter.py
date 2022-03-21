db7bc234c5f85de8307747bd9dea8fb9de5f7f6a
Debyte <74857321+DebyO2@users.noreply.github.com>
2022-03-21 12:05:56 +0530

    Update converter.py

--- playBoi V1/converter.py
+++ playBoi V1/converter.py
@@ -2,10 +2,6 @@ import os
 import subprocess
 def convert(path,name):
     # subprocess.call(['ffmpeg', '-i' ,path, '-ac', '2', '-f', 'mp3', f'{name}.mp3'])
-    subprocess.call(['ffmpeg', '-i', path, '-b:a', '192K', '-vn', f'{name}.mp3'])
+    subprocess.call(['ffmpeg', '-i', path, '-b:a', '192K', '-vn', f'{name}.mp3'],shell=True)
     os.remove(path)
     # os.system('cls')
-
-
-# convert(r'music\G9 - Wannabe (Audio).mp4',r'music\G9 - Wannabe (Audio)')
-

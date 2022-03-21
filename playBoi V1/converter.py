365a482f7d08ed75e41a96beae18d7477e618b65
Debyte <74857321+DebyO2@users.noreply.github.com>
2022-03-21 14:25:01 +0530

    Update converter.py

--- playBoi V1/converter.py
+++ playBoi V1/converter.py
@@ -1,12 +1,7 @@
-import ffmpeg
+from moviepy.editor import *
 import os
-import time
-def convert(path,name,destination):
-    video = ffmpeg.input(path)
-
-    audio = video.audio
-
-    stream = ffmpeg.output(audio, os.path.join(destination,name+".mp3"))
-
-    ffmpeg.run(stream)
-    os.remove(path)
+def convert(mp4, mp3):
+    mp4_without_frames = AudioFileClip(mp4)     
+    mp4_without_frames.write_audiofile(mp3)    
+    mp4_without_frames.close() # function call mp4_to_mp3("my_mp4_path.mp4", "audio.mp3")
+    os.remove(mp4)

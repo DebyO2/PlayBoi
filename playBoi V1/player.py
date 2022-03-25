66975f417ccf1c01523d4e0ea49de99fe5b277b0
Debyte <74857321+DebyO2@users.noreply.github.com>
2022-03-25 13:09:31 +0530

    optimised the code a lil bit lmao

--- playBoi V1/player.py
+++ playBoi V1/player.py
@@ -1,44 +1,49 @@
 import pygame
 import keyboard
 from win32gui import GetWindowText, GetForegroundWindow
+import os
+
 pygame.init()
 running = True
 paused = False
 name = GetWindowText(GetForegroundWindow())
 
-def toggled():
+
+def toggle(Global : bool):
     global paused
     global name
-    if(GetWindowText(GetForegroundWindow()) == name):
+    if Global:
+        if(GetWindowText(GetForegroundWindow()) == name):
+            if pygame.mixer.music.get_busy():
+                pygame.mixer.music.pause()
+                paused = True
+            else:
+                pygame.mixer.music.unpause()
+                paused = False
+            
+    else :
         if pygame.mixer.music.get_busy():
             pygame.mixer.music.pause()
             paused = True
         else:
             pygame.mixer.music.unpause()
-            paused = False        
-    
-def leave():
+            paused = False
+
+def leave(Global : bool):
     global running
     global name
-    if(GetWindowText(GetForegroundWindow()) == name):
+    
+    if Global:
+        if(GetWindowText(GetForegroundWindow()) == name):
+            running = False
+            pygame.mixer.music.stop()
+            
+    else :
         running = False
         pygame.mixer.music.stop()
 
-def toggledchk():
-    global paused
-    if pygame.mixer.music.get_busy():
-        pygame.mixer.music.pause()
-        paused = True
-    else:
-        pygame.mixer.music.unpause()
-        paused = False        
-    
-def leavechk():
-    global running
-    running = False
-    pygame.mixer.music.stop()
 
-def playSong(path : str,volume : int,Toloop : int):
+def playSong(path : str,volume : int,Toloop : int,offline :bool):
     global paused
     global running
     running = True
@@ -46,26 +51,41 @@ def playSong(path : str,volume : int,Toloop : int):
     print("shift + spacebar : pause/unpause (global shortcut)")
     print("esc              : stop          (within the window)")
     print("shift + esc      : stop          (global shortcut)\n")
-    pygame.mixer.music.load(path)
-    pygame.mixer.music.set_volume(volume)
-    pygame.mixer.music.play(Toloop)
-    keyboard.add_hotkey('space', toggled)
-    keyboard.add_hotkey('escape', leave)
-    keyboard.add_hotkey('shift+space', toggledchk)
-    keyboard.add_hotkey('shift+escape', leavechk)
+    try :
+        pygame.mixer.music.load(path)
+        pygame.mixer.music.set_volume(volume)
+        pygame.mixer.music.play(Toloop)
+    except:
+        print("oops looks like the song doesn't exist are u drunk or something? ")
+        return
+    keyboard.add_hotkey('space', toggle,args=[(True)])
+    keyboard.add_hotkey('escape', leave,args=[(True)])
+    keyboard.add_hotkey('shift+space', toggle,args=[(False)])
+    keyboard.add_hotkey('shift+escape', leave,args=[(False)])
    
     while running:              
         if paused or pygame.mixer.music.get_busy():
-            continue
+            continue   
         else:
+            print("finished playing")
             break
-    print("finished playing")
+    
+
     pygame.mixer.music.stop()
     pygame.mixer.music.unload()
     keyboard.unhook_all_hotkeys()
-    
+
+    if offline:
+        pass
+    else:
+        todelete = input("save song? (y/n) : ")
+        if todelete == "y":
+            print("saved")
+        elif todelete == "n":
+            os.remove(path)
+            print("deleted")
 if __name__ == '__main__':
     porth = input("Enter the path of the song: ")
     volune = int(input("Enter the volume(1-100): "))/100
     looop = int(input("To Loop? (0||1): "))*-1
-    playSong(porth,volune,looop)
+    playSong(porth,volune,looop,offline=True)
